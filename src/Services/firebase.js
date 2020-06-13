6d4d58f3df6c4beac20c82b67aecd1be3d0d1f19
Serge Modin <modinsv@yandex.ru>
2020-06-13 09:42:36 +0300

    Create firebase.js
    Firebase connected and set up through Firebase Console

---
+++ src/Services/firebase.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import * as firebase from 'firebase'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBq5tEW7yNLTm6x6gFcQZYY88jOkXbsJYc",
+  authDomain: "chatty-d2006.firebaseapp.com",
+  databaseURL: "https://chatty-d2006.firebaseio.com",
+  projectId: "chatty-d2006",
+  storageBucket: "chatty-d2006.appspot.com",
+  messagingSenderId: "586383282146",
+  appId: "1:586383282146:web:93c979f75f8a88c5fbb3bb",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  
+export default firebase
