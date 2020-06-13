a0f2aada216b4a0708544f123b2c0814540ccc45
Serge Modin <modinsv@yandex.ru>
2020-06-13 14:23:37 +0300

    Added SignUp page and login form in it

---
+++ src/Pages/Login/LoginStrings.js
@@ -0,0 +1,16 @@
+const LoginString = {
+    ID: 'id',
+    PhotoURL: 'photoUrl',
+    Name: 'nickname',
+    Email: 'email',
+    Password: 'iamge/',
+    DOC: 'added',
+    Description: 'mydescription',
+    FirebaseDocumentId: 'docid',
+    UPLOAD_CHANGED: 'state_changed',
+    PREFIX_IMAGE: 'iamge/',
+    CurrentUserdocKey:''
+
+}
+
+export default LoginString;
