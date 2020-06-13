4c0e542bd7b974dd05e496c7ecc822ecdc32085c
Serge Modin <modinsv@yandex.ru>
2020-06-13 12:40:34 +0300

    Basic configure
    - Added fontawsome
    - Added grids-responsive-min.css
    - Added toasts
    -Added Routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './Pages/Home/Home'
+import Signup from './Pages/Signup/Signup'
+import Login from './Pages/Login/Login'
+import Chat from './Pages/Chat/Chat'
+import firebase from './Services/firebase'
+import { toast, ToastContainer } from 'react-toastify'
+
+class App extends Component {
+
+
+  showToast = (type, message) => {
+    switch (type) {
+      case 0:
+        toast.warning(message)
+        break;
+      case 1:
+        toast.success(message)
+      default:
+        break;
+    }
+  }
+  constructor() {
+    super();
+    this.state = {
+      authenticated: false,
+      loading: true
+    }
+  }
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          loading:false
+        })
+      } else {
+        this.setState({
+          authenticated: false,
+          loading:false
+        })
+      }
+    })
+  }
+
+  render() {
+    return this.state.loading === true ? (
+      <div className='spinner-border text-success' role='status'>
+        <span className='sr-only'> Loading ....</span>
+      </div>
+    ) : (
+      <Router>
+        <ToastContainer
+          autoClose={2000}
+          hideProgressBar={true}
+          position={toast.POSITION.BOTTOM_CENTER}
+        />
+        <Switch>
+          <Route exact path='/' render={(props) => <Home {...props} />} />
+          <Route
+            path='/login'
+            render={(props) => <Login showToast={this.showToast} {...props} />}
+          />
+          <Route
+            path='/signup'
+            render={(props) => <Signup showToast={this.showToast} {...props} />}
+          />
+          <Route
+            path='/chat'
+            render={(props) => <Chat showToast={this.showToast} {...props} />}
+          />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
-export default App;
+export default App
